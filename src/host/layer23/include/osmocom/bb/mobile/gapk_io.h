f51ed5b1bae42e33ce31c4c6a2ec6f08f31aa2c7
Vadim Yanitskiy <axilirator@gmail.com>
2017-12-12 19:14:13 +0700

    host/mobile: integrate GAPK based audio I/O back-end
    This change introduces a new feature of the mobile application -
    audio I/O support, which allows one to speak right from PC running
    mobile through its ordinary mic and speakers.
    
    The audio I/O is based on GAPK library, which relays on ALSA sound
    framework. The API of GAPK implies to use the processing queues
    (chains), which basically consist of a source block, several
    processing blocks, and a sink block. So, there are two voice
    processing chains:
    
      - 'pq_audio_source' (voice capture -> frame encoding),
      - 'pq_audio_sink' (frame decoding -> voice playback).
    
    Both of them exchange frames from two dedicated buffers:
    
      - 'tch_fb_ul' - a buffer for to be played DL TCH frames,
      - 'tch_fb_dl' - a buffer for encoded UL TCH frames.
    
    In its turn, both buffers are served by a new gapk_io_dequeue()
    function, which is being called within the mobile_work() loop.
    
    Current limitations:
    
      - TCF/FR codec support only. Despite the both chains are
        codec independent, they are being initialized with FR.
        Should be resolved together with the first issue.
    
    Change-Id: Ib86b0746606c191573cc773f01172afbb52f33a9

---
+++ src/host/layer23/include/osmocom/bb/mobile/gapk_io.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <osmocom/gapk/procqueue.h>
+#include <osmocom/gapk/codecs.h>
+
+/* Forward declarations */
+struct osmocom_ms;
+
+struct gapk_io_state {
+	/* src/alsa -> proc/codec -> sink/tch_fb */
+	struct osmo_gapk_pq *pq_source;
+	/* src/tch_fb -> proc/codec -> sink/alsa */
+	struct osmo_gapk_pq *pq_sink;
+
+	/* Description of a codec, that is currently used */
+	const struct osmo_gapk_codec_desc *codec_desc;
+
+	/* Buffer for to be played TCH frames (from DL) */
+	struct llist_head tch_fb_dl;
+	/* Buffer for encoded TCH frames (for UL) */
+	struct llist_head tch_fb_ul;
+};
+
+void gapk_io_init(void);
+int gapk_io_dequeue(struct osmocom_ms *ms);
+
+int gapk_io_init_ms_chan(struct osmocom_ms *ms,
+	uint8_t ch_type, uint8_t ch_mode);
+int gapk_io_init_ms(struct osmocom_ms *ms,
+	enum osmo_gapk_codec_type codec);
+int gapk_io_clean_up_ms(struct osmocom_ms *ms);
